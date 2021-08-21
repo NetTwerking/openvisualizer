1e903cfab76675eca7b024025c0844f65298a35d
yuhwan97 <yuhwan9706@gmail.com>
2021-08-21 17:04:01 +0900

    Clicker info -> web
    MAC, answer 정보를 csv파일로 저장 후
    웹에서 csv 파일을 읽어 표시

--- openvisualizer/moteConnector/web.py
+++ openvisualizer/moteConnector/web.py
@@ -1,34 +1,48 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 # from STU_Notice import get_stu_notices
 import csv
+
+from werkzeug.utils import redirect
 app = Flask(__name__)
+
+mac_list = ['0x15']
+
+
 
 @app.route('/', methods=['GET','POST'])
 @app.route('/index', methods=['GET','POST'])
 def Show_Clicker():
-    import ParserData
-    COUNT = ParserData.ParserData.COUNT
-    return render_template('index.html', count = COUNT)
+    return render_template('index.html')
 
 @app.route('/quiz', methods=['GET','POST'])
 def Show_Quiz():
-    #f = open('C:\DelayTest\clicker.csv', 'r')
-    #rdr = csv.reader(f)
-    #data = []
-    #for i in rdr:
-        #data.append(i)
+    if request.method == 'POST':
+        _question = request.form['question']
+        _option1 = request.form['option1']
+        _option2 = request.form['option2']
+        return render_template('start_quiz.html', question=_question, option1 = _option1, option2 = _option2)
+    elif request.method == 'GET':
+        return render_template('quiz.html')
 
-    return render_template('quiz.html')
-
+@app.route('/start_quiz', methods=['GET','POST'])
+def Start_Quiz():
+    _question = request.form['question']
+    _option1 = request.form['option1']
+    _option2 = request.form['option2']
+    return render_template('start_quiz.html', question=_question, option1 = _option1, option2 = _option2)
+   
 @app.route('/results', methods=['GET','POST'])
 def Show_Result():
-    #f = open('C:\DelayTest\clicker.csv', 'r')
-    #rdr = csv.reader(f)
-    #data = []
-    #for i in rdr:
-        #data.append(i)
-
-    return render_template('results.html') 
+    result_list = {}
+    for mac in mac_list:
+        read_dir = 'C:\\DelayTest\\' + mac + '.csv'
+        f = open(read_dir, 'r')
+        csv_reader = csv.reader(f)
+        for line in csv_reader :
+            print(line[1])
+            result_list[mac] = line[1]
+        f.close()
+    return render_template('results.html', result_list = result_list, mac_list = mac_list) 
 
 if __name__ == '__main__':
     app.run()
