5a5002397b2f52147e6c5d0cc2800c2edaa2c915
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2021-09-23 02:29:46 +0900

    그래프 수정
    질문이랑 보기 라벨링

--- openvisualizer/moteConnector/web.py
+++ openvisualizer/moteConnector/web.py
@@ -6,13 +6,21 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
+from matplotlib import rc
+
 
 from werkzeug.utils import redirect
 import time
 
+rc('font', family='AppleGothic')
+
+plt.rcParams['axes.unicode_minus'] = False
+
 app = Flask(__name__)
 
 mac_list = ['0x15','0xa1','0x65','0xee','0xe8','0xa4']
+x_label = ["o", "x"]
+quiz = []
 
 @app.route('/', methods=['GET','POST'])
 @app.route('/index', methods=['GET','POST'])
@@ -33,8 +41,11 @@ def Show_Quiz():
 @app.route('/start_quiz', methods=['GET','POST'])
 def Start_Quiz():
     _question = request.form['question']
+    quiz.append(_question)
     _option1 = request.form['option1']
+    x_label[0] = _option1
     _option2 = request.form['option2']
+    x_label[1] = _option2
     _time = request.form['quiz_time']
     now = time.localtime()
     f5 = open('/Users/songjihye/Desktop/time.txt', 'w')
@@ -65,7 +76,7 @@ def plot():
 
     #통계 내기
     x_data = [1, 2]
-    x_label = ['o', 'x']
+    #x_label = [option_o, option_x]
     colors = ['#0d6efd', '#ff9999']
 
     o = 0
@@ -99,6 +110,7 @@ def plot():
     ratio.append(int(x/(o+x)*100))
     explode = [0.05, 0.05]
 
+    plt.title(quiz[0])
     axis.pie(ratio, labels=x_label, colors=colors, explode=explode, shadow=True, autopct='%.1f%%')
 
     # 그려진 img 파일 내용을 html 랜더링 쪽에 전송한다.
